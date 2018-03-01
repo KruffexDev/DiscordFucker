9b83a90e741969d3701d69eb7ca48cff8c371417
KruffexDev <36914873+KruffexDev@users.noreply.github.com>
2018-03-01 11:46:45 +0200

    a simple bomber bot for discord

--- DiscordBomber.js
+++ DiscordBomber.js
@@ -1,1 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.client();
 
+client.on('message', message => {
+  if (message.content.includes("hello", "hi", "howdy", "hey")) {
+    message.reply('Hi! You got pranked');
+  }
+});
+client.login("YOUR SECRET BOT TOKEN INSTEAD OF THIS BULLSHIT TEXT");
